82735cd31c95c816c4d9670c40078d79ea25d3d9
Fernando Orsa <u20161c624@upc.edu.pe>
2018-05-01 22:47:01 -0500

    .

---
+++ TAGO_CABIFY/Dominio/Chofer.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Runtime.Serialization;
+
+namespace TAGO_CABIFY.Dominio
+{
+    [DataContract]
+    public class Chofer
+    {
+        [DataMember(IsRequired = false)]
+        public string nombres { get; set; }
+
+        [DataMember(IsRequired = false)]
+        public string apellidos { get; set; }
+
+        [DataMember(IsRequired = false)]
+        public string celular { get; set; }
+
+        [DataMember]
+        public string ModeloAuto { get; set; }
+
+        [DataMember]
+        public int reputacion { get; set; }
+
+        [DataMember]
+        public DateTime FechaRegistro { get; set; }
+
+    }
+}
