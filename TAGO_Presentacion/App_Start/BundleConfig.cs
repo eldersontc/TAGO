194983c7b8a3b8e4bcea0cedb1e71b0ceac157ee
S76897 <S76897@PP02GL38820WS20>
2018-05-04 17:12:23 -0500

    Se agregó la grilla para listar los conductores disponibles.

--- TAGO_Presentacion/App_Start/BundleConfig.cs
+++ TAGO_Presentacion/App_Start/BundleConfig.cs
@@ -14,6 +14,9 @@ namespace TAGO_Presentacion
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
                         "~/Scripts/jquery.validate*"));
 
+            bundles.Add(new ScriptBundle("~/bundles/jquery.base64").Include(
+                        "~/Scripts/jquery.base64.js"));
+
             // Utilice la versión de desarrollo de Modernizr para desarrollar y obtener información. De este modo, estará
             // preparado para la producción y podrá utilizar la herramienta de compilación disponible en http://modernizr.com para seleccionar solo las pruebas que necesite.
             bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
--- TAGO_Presentacion/Controllers/ManageController.cs
+++ TAGO_Presentacion/Controllers/ManageController.cs
@@ -1,8 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text;
 using System.Web;
 using System.Web.Mvc;
+using TAGO_Presentacion.Models;
 
 namespace TAGO_Presentacion.Controllers
 {
@@ -14,9 +16,39 @@ namespace TAGO_Presentacion.Controllers
             return View();
         }
 
-        public ActionResult Elegir(string origen, string destino)
+        public ActionResult Elegir(string o, string d)
         {
+            string i = Encoding.UTF8.GetString(Convert.FromBase64String(o));
+            string f = Encoding.UTF8.GetString(Convert.FromBase64String(d));
+            string[] arri = i.Split('|');
+            string[] arrf = f.Split('|');
+            ViewBag.PO = arri[0];
+            ViewBag.PD = arrf[0];
             //ViewBag.NombreCliente = ((ClienteService.Cliente)Session["clienteLogueado"]).Nombres;
+            List<CandidatoModel> candidatos = new List<CandidatoModel>();
+            candidatos.Add(new CandidatoModel {
+                Proveedor = "UBER",
+                Conductor = "ELDERSON TABOADA",
+                MarcaVehiculo = "TOYOTA",
+                PlacaVehiculo = "ABC-123",
+                Distancia = 30,
+                Precio = 15
+            });
+            candidatos.Add(new CandidatoModel
+            {
+                Proveedor = "CABIFY",
+                Conductor = "JUAN SANCHEZ",
+                MarcaVehiculo = "KIA",
+                PlacaVehiculo = "DEF-456",
+                Distancia = 20,
+                Precio = 20
+            });
+            return View(candidatos);
+        }
+        
+        public ActionResult Pagar(string proveedor, string placa)
+        {
+            
             return View();
         }
     }
