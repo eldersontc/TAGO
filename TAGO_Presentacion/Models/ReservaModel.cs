951078a170ca4ae7d00650c859312b8b36061ea4
Elderson Taboada Chavez <etaboada@outlook.com>
2018-05-05 18:47:00 -0500

    Integracion 2

---
+++ TAGO_Presentacion/Models/ReservaModel.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace TAGO_Presentacion.Models
+{
+    public class ReservaModel
+    {
+        [Required]
+        public string Origen { get; set; }
+        [Required]
+        public string Destino { get; set; }
+        [Required]
+        public string Conductor { get; set; }
+        [Required]
+        public string AutoModelo { get; set; }
+        [Required]
+        public string AutoPlaca { get; set; }
+        [Required]
+        public int Precio { get; set; }
+
+        public ClienteService.Tarjeta[] Tarjetas { get; set; }
+
+        public string TarjetaSeleccionada { get; set; }
+    }
+}
